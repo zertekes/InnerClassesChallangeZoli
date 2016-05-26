3fcff331414b5ea668d52f30abfa7e5892980d35
Zoltan Ertekes <zertekes@gmail.com>
2016-05-27 00:04:21 +0100

    upload

---
+++ src/com/nazoweb/Album.java
@@ -0,0 +1,98 @@
+package com.nazoweb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zertekes on 05/04/2016.
+ */
+public class Album {
+
+    private String name;
+    private String artist;
+    private SongList songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new SongList();
+
+
+    }
+
+
+    public boolean addSong(String title, double duration) {
+
+        return    this.songs.add(new Song(title, duration));
+
+    }
+
+//    private Song findSong(String title) {
+//        for(Song checkedSong: this.songs) {
+//            if(checkedSong.getTitle().equals(title)) {
+//                return checkedSong;
+//            }
+//        }
+//        return null;
+//    }
+
+    public boolean addToPlayList(int trackNumber, List<Song> playList) {
+        Song checkedSong = this.songs.findSong(trackNumber);
+        if(checkedSong != null) {
+            playList.add(checkedSong);
+            return true;
+
+        }
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList(String title, List<Song> playList) {
+        Song checkedSong = songs.findSong(title);
+        if(checkedSong != null) {
+            playList.add(checkedSong);
+            return true;
+        }
+        System.out.println("The song " + title + " is not in this album");
+        return false;
+    }
+
+
+    private class SongList {
+
+        private ArrayList<Song> songs;
+
+        public SongList() {
+            this.songs = new ArrayList<Song>();
+        }
+
+        public boolean add(Song song) {
+            if (songs.contains(song)) {
+                return false;
+            }
+            this.songs.add(song);
+            return true;
+        }
+
+        private Song findSong(String title) {
+            for (Song checkedSong : this.songs) {
+                if (checkedSong.getTitle().equals(title)) {
+                    return checkedSong;
+                }
+            }
+            return null;
+        }
+
+        private Song findSong(int trackNumber) {
+            int index = trackNumber - 1;
+            if ((index > 0) && (index <= this.songs.size())) {
+                return songs.get(index);
+            }
+            return null;
+        }
+
+
+    }
+
+
+}
---
+++ src/com/nazoweb/Song.java
@@ -0,0 +1,24 @@
+package com.nazoweb;
+
+/**
+ * Created by zertekes on 05/04/2016.
+ */
+public class Song {
+
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+}
